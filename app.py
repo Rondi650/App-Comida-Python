2928b787e465a4e20ac430de9c7385bc06fdbbd5
Rondi650 <rondinellenunes123@gmail.com>
2025-08-13 20:42:20 -0300

    inclusao metodo de media

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from Modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
-
 restaurante_mexicano.alternar_estado()
+restaurante_praca.receber_avaliacao('Rondi',8)
+restaurante_praca.receber_avaliacao('Samara',5)
+
+
 
 def main():
     Restaurante.listar_restaurantes()
