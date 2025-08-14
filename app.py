a4211604e0cf677c2bd115932e50001dc324dcc8
Rondi650 <rondinellenunes123@gmail.com>
2025-08-14 15:43:26 -0300

    Heranca

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from Modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+
 restaurante_mexicano.alternar_estado()
 restaurante_praca.receber_avaliacao('Rondi',8)
 restaurante_praca.receber_avaliacao('Samara',5)
