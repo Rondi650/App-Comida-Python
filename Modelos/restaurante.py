8be9845511c61e818b49be161bb09d5580220c2b
Rondi650 <rondinellenunes123@gmail.com>
2025-08-13 21:06:21 -0300

    incluida avaliacao terminal

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -15,9 +15,9 @@ class Restaurante:
      
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliacoes'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacao).ljust(25)} | {restaurante.ativo}')
             
     @property
     def ativo(self):
